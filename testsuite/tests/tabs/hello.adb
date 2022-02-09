c2fb9c188a64b2de43bfda7b74c346670e0b3e4a
Nicolas Setton <setton@adacore.com>
2022-02-09 12:50:44 +0000

    Fix support for tabs
    Do this by removing all tabs from files - if the issue disappears
    after removal of tabs, give up and ask the user to remove tabs.
    
    Fixes #5

---
+++ testsuite/tests/tabs/hello.adb
@@ -0,0 +1,8 @@
+with Ada.Text_IO;
+procedure Hello is
+         p : integer;
+begin
+		P := 2;
+	   P := 3; 	P := 4;
+	   Ada.Text_IO.Put_Line ("hello");
+end Hello;
---
+++ testsuite/tests/tabs/test.sh
@@ -0,0 +1,2 @@
+$ADAREDUCER --single-file hello.adb p.gpr oracle.sh > /dev/null
+cat hello.adb
