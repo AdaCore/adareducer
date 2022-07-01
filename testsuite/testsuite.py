84d51d571a9a0d8e01b1c79ad08d07a1fe73bfab
Pierre-Marie de Rodat <derodat@adacore.com>
2022-07-01 10:53:23 +0200

    Testsuite: add a --no-auto-path option
    In the production environment, we want the testsuite to exercize the
    adareducer that is being packaged (installed in the environment), not
    the adareducer that is part of the testsuite source tree.
    
    This new option prevents the testsuite from automatically adding the
    adareducer part of the testsuite source tree to the environment.
    
    TN: V317-010

--- testsuite/testsuite.py
+++ testsuite/testsuite.py
@@ -21,17 +21,32 @@ class AdaReducerTestsuite(e3.testsuite.Testsuite):
     test_driver_map = {"shell_script": ShellScriptDriver}
     default_driver = "shell_script"
 
+    def add_options(self, parser):
+        parser.add_argument(
+            "--no-auto-path",
+            dest="auto_path",
+            action="store_false",
+            help="Do not automatically make adareducer available, i.e. assume"
+            " it is already available in the environment."
+        )
+
     def set_up(self) -> None:
         super().set_up()
 
-        # Set PYTHONPATH to find adareducer
-        root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-        os.environ["PYTHONPATH"] = "{}:{}".format(
-            root_dir,
-            os.environ.get("PYTHONPATH", ""),
-        )
+        if self.env.options.auto_path:
+            # Set PYTHONPATH to find adareducer
+            root_dir = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), "..")
+            )
+            os.environ["PYTHONPATH"] = "{}:{}".format(
+                root_dir,
+                os.environ.get("PYTHONPATH", ""),
+            )
 
-        os.environ["ADAREDUCER"] = f"{root_dir}/adareducer"
+            os.environ["ADAREDUCER"] = f"{root_dir}/adareducer"
+
+        else:
+            os.environ["ADAREDUCER"] = "adareducer"
 
 if __name__ == "__main__":
     sys.exit(AdaReducerTestsuite(os.path.dirname(__file__)).testsuite_main())
